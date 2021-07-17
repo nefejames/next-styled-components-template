e999e244d8ed84d2c556d6a88700f318d4feb810
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-07-17 10:22:21 -0700

    starter files

---
+++ README.md
@@ -0,0 +1,1 @@
+A basic template with for how I work with Next.js and Styled components. I sometimes use Frmer Motion, so I includd that too.
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next && Styled Components </title>
+      </Head>
+
+      <h1>Let&apos;s go</h1>
+    </div>
+  );
+}
